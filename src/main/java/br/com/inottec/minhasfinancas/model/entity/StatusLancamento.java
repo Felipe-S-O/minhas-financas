026c9bb501205546de73a6424e92a6e7336005c2
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2022-03-30 06:54:50 -0300

    criando  entidades
    criando entidade usuário e lançamento

---
+++ src/main/java/br/com/inottec/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,9 @@
+package br.com.inottec.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+
+}
---
+++ src/main/java/br/com/inottec/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,9 @@
+package br.com.inottec.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+	
+	
+	RECEITA,
+	DESPESA
+
+}
